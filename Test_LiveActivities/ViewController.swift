05357f10a86bd60944a8ec1e204df67af20e531a
陈征 <jiajun12_03@163.com>
2022-11-08 14:55:56 +0800

    Initial Commit

---
+++ Test_LiveActivities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_LiveActivities
+//
+//  Created by mac on 2022/11/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
